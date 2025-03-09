5521a0daf28ce30ace47277843d833ff672978b6
rfleisc6 <143972997+rfleisc6@users.noreply.github.com>
2025-03-09 17:40:41 -0400

    sucessfully looks up a user based on the return value from the authenticator

--- backend/userdatabase.py
+++ backend/userdatabase.py
@@ -1,11 +1,44 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://pythhon:8x5pGda7EWdKGtV0@nomcents.5fhvz.mongodb.net/?retryWrites=true&w=majority&appName=nomcents"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+import json
+
+##the authenticator produces output of form 
+##    { "id": "1234", "given_name": "John", "name": "John Doe", "email": "john_doe@idp.example" }
+## for now we can just store that whole thing, we will use the email as the indexing entity tho because theoretically we might want to support email password logins
+##
+ATLAS_URI="mongodb+srv://pythhon:8x5pGda7EWdKGtV0@nomcents.5fhvz.mongodb.net/?retryWrites=true&w=majority&appName=nomcents"
+DB_NAME = 'users'
+COLLECTION_NAME = 'users_test'
+class AtlasClient ():
+   def __init__ (self, altas_uri, dbname):
+       self.mongodb_client = MongoClient(altas_uri)
+       self.database = self.mongodb_client[dbname]
+   ## A quick way to test if we can connect to Atlas instance
+   def ping (self):
+       self.mongodb_client.admin.command('ping')
+   def get_collection (self, collection_name):
+       collection = self.database[collection_name]
+       return collection
+   def find (self, collection_name, filter = {}, limit=0):
+       collection = self.database[collection_name]
+       items = list(collection.find(filter=filter, limit=limit))
+       return items
+   def userLookup (self,userString,collection_name):
+       userdet=json.loads(userString)
+       collection = self.database[collection_name]
+       user = collection.find_one({"email":userdet['email']})
+       return user
+   
+
+
+
+
+atlas_client = AtlasClient (ATLAS_URI, DB_NAME)
+atlas_client.ping()
+print ('Connected to Atlas instance! We are good to go!')
+user = atlas_client.userLookup (collection_name=COLLECTION_NAME, userString='{"email":"'+ input("Enter email: ")+'\"}')
+if(not (user is None)):
+    print("username is "+ user['name'])
+else:
+    print("cannot find user")
+    
