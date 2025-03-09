9a567df8a2380c45443bc312fdbd74712a034854
rfleisc6 <143972997+rfleisc6@users.noreply.github.com>
2025-03-09 18:31:28 -0400

    aditionally able to create new users

--- backend/userdatabase.py
+++ backend/userdatabase.py
@@ -23,22 +23,47 @@ class AtlasClient ():
        collection = self.database[collection_name]
        items = list(collection.find(filter=filter, limit=limit))
        return items
-   def userLookup (self,userString,collection_name):
-       userdet=json.loads(userString)
+   def insWrapper(self,newDoc,collection_name):
        collection = self.database[collection_name]
+       collection.insert_one(newDoc)
+       
+   
+class datauser():
+    def __init__(self,atlas_uri, dbname, collection_name):
+        self.atlas_client = AtlasClient (ATLAS_URI, DB_NAME)
+        self.atlas_client.ping()
+        print ('Connected to Atlas instance! We are good to go!')
+        self.collection_name=collection_name
+    def userLookup (self,userString):
+       userdet=json.loads(userString)
+       collection = self.database[self.collection_name]
        user = collection.find_one({"email":userdet['email']})
        return user
-   
+    def isUser(self,userstring):
+        user = self.userLookup (userString='{"email":"'+ input("Enter email: ")+'\"}')
+        if(not (user is None)):
+            return True
+        else:
+            return False
+    def makeUser(self,email,name):
+        usstring=('{"email":"'+ email+'\"}')
+        if(not data.isUser(usstring)):
+            self.atlas_client.insWrapper({"name":name, "email":email})
+        else:
+            print("failure user exists")
 
 
 
 
-atlas_client = AtlasClient (ATLAS_URI, DB_NAME)
-atlas_client.ping()
+data = datauser (ATLAS_URI, DB_NAME,COLLECTION_NAME)
 print ('Connected to Atlas instance! We are good to go!')
-user = atlas_client.userLookup (collection_name=COLLECTION_NAME, userString='{"email":"'+ input("Enter email: ")+'\"}')
-if(not (user is None)):
+email=input("Enter email: ")
+usstring=('{"email":"'+ email+'\"}')
+if(data.isUser(usstring)):
+    user=data.userLookup(usstring)
     print("username is "+ user['name'])
 else:
-    print("cannot find user")
-    
+    print("user not found, create user now")
+    name = input("Enter name: ")
+    data.makeUser(email,name)
+print(data.isUser(usstring))
