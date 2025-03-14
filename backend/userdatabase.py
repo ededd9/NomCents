5b197b9c494e50d0768e1e27b267da05154adf0b
rfleisc6 <143972997+rfleisc6@users.noreply.github.com>
2025-03-14 15:02:01 -0400

    the database logic works i need to test taht i formatted the http stuff right

--- backend/userdatabase.py
+++ backend/userdatabase.py
@@ -1,7 +1,9 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import json
-
+from flask import Flask, abort,request
+from flask_cors import CORS
+import requests, math
 ##the authenticator produces output of form 
 ##    { "id": "1234", "given_name": "John", "name": "John Doe", "email": "john_doe@idp.example" }
 ## for now we can just store that whole thing, we will use the email as the indexing entity tho because theoretically we might want to support email password logins
@@ -26,6 +28,9 @@ class AtlasClient ():
    def insWrapper(self,newDoc,collection_name):
        collection = self.database[collection_name]
        collection.insert_one(newDoc)
+   def upWrapper(self,eDock,collection_name):
+       collection = self.database[collection_name]
+       collection.update_one({"email":eDock['email']},{"$set": eDock}) 
        
    
 class datauser():
@@ -49,20 +54,65 @@ class datauser():
         userdet=json.loads(userstring)
         if(not data.isUser(userstring)):
             self.atlas_client.insWrapper(userdet,self.collection_name)
+            return True
         else:
-            print("failure user exists")
+            return False
+    def editUser(self,userstring):
+        userdet=json.loads(userstring)
+        if(not data.isUser(userstring)):
+            return False
+        else:
+            self.atlas_client.upWrapper(userdet,self.collection_name)
+            return True;
 
 
+
+# data = datauser (ATLAS_URI, DB_NAME,COLLECTION_NAME)
+# email=input("Enter email: ")
+# usstring=('{"email":"'+ email+'"}')
+# if(data.isUse"emailr(usstring)):
+#     user=data.userLookup(usstring)
+#     print("user name is "+ user['name'])
+#     usstrindg='{"email":"'+ email+'", "name":"'+user['name']+'e"}'
+#     data.editUser(usstrindg)
+#     user=data.userLookup(usstring)
+#     print("user name is "+ user['name'])
+# else:
+#     print("user not found, create user now")
+#     name = input("Enter name: ")
+#     usstring='{"email":"'+ email+'", "name":"'+name+'"}'
+
+#     data.makeUser(usstring)
+# print(data.isUser(usstring))
+
+app = Flask(__name__)
 data = datauser (ATLAS_URI, DB_NAME,COLLECTION_NAME)
-email=input("Enter email: ")
-usstring=('{"email":"'+ email+'"}')
-if(data.isUser(usstring)):
-    user=data.userLookup(usstring)
-    print("user name is "+ user['name'])
-else:
-    print("user not found, create user now")
-    name = input("Enter name: ")
-    usstring='{"email":"'+ email+'", "name":"'+name+'"}'
 
-    data.makeUser(usstring)
-print(data.isUser(usstring))
+CORS(app) # allows cross-origin requests, which is needed for the frontend to access the backend
+@app.route('/api/user', methods=['GET', 'POST', 'PUT'])
+def user():
+    if request.method == 'GET':
+        email = request.args['email']
+        usstring=('{"email":"'+ email+'"}')
+        user=data.userLookup(usstring)
+        if(not (user is None)):
+            return json.dump(user)
+        else:
+            abort(404)
+    elif request.method== 'POST':
+        usstring=request.args['user']
+        if data.makeUser(usstring):
+            return usstring
+        else:
+            abort(404)
+    elif request.method== 'PUT':
+        usstring=request.args['user']
+        if data.editUser(usstring):
+            return usstring
+        else:
+            abort(404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
