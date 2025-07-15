13ece55f9e3d5ebd0aa9b02b8b2b827f1e45ffe3
Edward Hurtado <edhur9@gmail.com>
2025-07-15 13:19:10 -0400

    changed a tags to Links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes, Link } from "react-router-dom";
 import "./App.css";
 import { LoginProvider, LoginContext } from "./contexts/LoginContext";
 import Homepage from "./pages/Homepage";
